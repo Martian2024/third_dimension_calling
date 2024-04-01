0dab22f91930da2efe5cdc5f2f6246c8e3516ac7
Arsenio <arseniy.kolomeytsev@gmail.com>
2024-04-02 01:29:31 +0300

    fucking hate git (almost lost results of three days of work)

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from typing import Union
 from math import sin, cos, tan, sqrt, atan, pi, radians, degrees
 
+def sign(x):
+    if x < 0:
+        return -1
+    return 1
+
 class Vector:
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
 
-    def scale(self, k):
+    '''def scale(self, k):
         self.x *= k
         self.y *= k
-        self.z *= k
+        self.z *= k'''
 
     def __mul__(self, k: Union[int, float]):
         return Vector(self.x * k, self.y * k, self.z * k)
+    
+    def length(self):
+        return sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
 
 
 class Point:
@@ -40,9 +48,10 @@ class Edge:
     
 
 class Camera:
-    def __init__(self, position: Point, angle: Union[int, float]):
+    def __init__(self, position: Point, angle_y: Union[int, float], angle_z: Union[int, float]):
         self.position = position
-        self.angle = angle
+        self.angle_y = angle_y
+        self.angle_z = angle_z
         self.vision = 60
         self.plane_side = 2 * tan(radians(self.vision))
 
@@ -58,10 +67,23 @@ class Camera:
         for i in points:
             try:
                 vector = Vector(i.x, i.y, i.z)
-                new_angle = degrees(atan(vector.z / vector.x)) - self.angle
+                new_angle = degrees(atan(vector.z / vector.x)) - self.angle_y
                 new_x = sqrt(vector.x ** 2 + vector.z ** 2) * cos(radians(new_angle))
                 new_z = sqrt(vector.x ** 2 + vector.z ** 2) * sin(radians(new_angle))
                 rotated_points.append(Point(new_x, i.y, new_z))
+            except ZeroDivisionError:
+                pass
+        return rotated_points
+    
+    def rotate_points_z(self, points): #по часовой стрелке - положительный угол
+        rotated_points = []
+        for i in points:
+            try:
+                vector = Vector(i.x, i.y, i.z)
+                new_angle = degrees(atan(vector.y / vector.x)) - self.angle_z
+                new_x = sqrt(vector.x ** 2 + vector.y ** 2) * cos(radians(new_angle))
+                new_y = sqrt(vector.x ** 2 + vector.y ** 2) * sin(radians(new_angle))
+                rotated_points.append(Point(new_x, new_y, i.z))
             except ZeroDivisionError:
                 pass
         return rotated_points
@@ -73,6 +95,7 @@ class Camera:
         return_list = []
         points = self.translate_points(points)
         points = self.rotate_points_y(points)
+        points = self.rotate_points_z(points)
         for i in points:
             vector = Vector(i.x, i.y, i.z)
             k = vector.x 
@@ -90,5 +113,103 @@ class Camera:
         for i in edges:
             return_list.append(self.render_points(i.points))
         return return_list
+    
+    def translate_to_new_basis(self, polygon, intersec_point, v1, v2):
+        v1_args = {'x': v1.x, 'y': v1.y, 'z': v1.z}
+        v2_args = {'x': v2.x, 'y': v2.y, 'z': v2.z}
+        intersec_point_args = {'x': intersec_point.x, 'y': intersec_point.y, 'z': intersec_point.z}
+        o_args = {'x': polygon.points[1].x, 'y': polygon.points[1].y, 'z': polygon.points[1].z}
+        if v1.x != 0:
+            c1 = 'x'
+            if v2.y != 0:
+                c2 = 'y'
+            else:
+                c2 = 'z'
+        elif v1.y != 0:
+            c1 = 'y'
+            if v2.x != 0:
+                c2 = 'x '                       #может наебнуться
+            else:
+                c2 = 'z'
+        else:
+            c1 = 'z'
+            if v2.x != 0:
+                c2 = 'x'
+            else:
+                c2 = 'y'
+        y = (intersec_point_args[c2] - (intersec_point_args[c1] - o_args[c1]) * v1_args[c2] / v1_args[c1]) / (-1 * (v2_args[c1] * v1_args[c2] / v1_args[c1] + v2_args[c2]))
+        x = (intersec_point_args[c1] - o_args[c1] - y * v2_args[c1]) / v1_args[c1]
+
+        return x, y
+    
+    def getDistance(self, polygon):
+        BA = Vector(polygon.points[1].x - polygon.points[0].x, polygon.points[1].y - polygon.points[0].y, polygon.points[1].z - polygon.points[0].z)
+        BC = Vector(polygon.points[2].x - polygon.points[1].x, polygon.points[2].y - polygon.points[1].y, polygon.points[2].z - polygon.points[1].z)
+        CA = Vector(polygon.points[2].x - polygon.points[0].x, polygon.points[2].y - polygon.points[0].y, polygon.points[2].z - polygon.points[0].z)
+        normal = Vector(BA.y * BC.z - BA.z * BC.y, BA.z * BC.x - BA.x * BC.z, BA.x * BC.y - BA.y * BC.x)
+        length = abs(normal.x * self.position.x + normal.y * self.position.y + normal.z * self.position.z - (normal.x * polygon.points[1].x + normal.y * polygon.points[1].y + normal.z * polygon.points[1].z)) / sqrt(normal.x ** 2 + normal.y ** 2 + normal.z ** 2)
+        normal = normal * (length / sqrt(normal.x ** 2 + normal.y ** 2 + normal.z ** 2))
+        antinormal = normal * -1
+
+        
+
+        intersec_point = self.position + normal
+        if normal.x * intersec_point.x + normal.y * intersec_point.y + normal.z * intersec_point.z - (normal.x * polygon.points[1].x + normal.y * polygon.points[1].y + normal.z * polygon.points[1].z) != 0:
+            intersec_point = self.position + antinormal
+        
+        #print(normal.x, normal.y, normal.z)
+        #print(intersec_point.x, intersec_point.y, intersec_point.z)
+            
+
+        #новая проверка на вхождение точки пересечения в треугольник
+        DA = Vector(intersec_point.x - polygon.points[0].x, intersec_point.y - polygon.points[0].y, intersec_point.z - polygon.points[0].z)
+        DB = Vector(intersec_point.x - polygon.points[1].x, intersec_point.y - polygon.points[1].y, intersec_point.z - polygon.points[1].z)
+        DC = Vector(intersec_point.x - polygon.points[2].x, intersec_point.y - polygon.points[2].y, intersec_point.z - polygon.points[2].z)
+
+        s_ABC = Vector(BA.y * BC.z - BA.z * BC.y, BA.z * BC.x - BA.x * BC.z, BA.x * BC.y - BA.y * BC.x).length() / 2
+        s_ABD = Vector(DA.y * DB.z - DA.z * DB.y, DA.z * DB.x - DA.x * DB.z, DA.x * DB.y - DA.y * DB.x).length() / 2
+        s_BCD = Vector(DB.y * DC.z - DB.z * DC.y, DB.z * DC.x - DB.x * DC.z, DB.x * DC.y - DB.y * DC.x).length() / 2
+        s_DCA = Vector(DA.y * DC.z - DA.z * DC.y, DA.z * DC.x - DA.x * DC.z, DA.x * DC.y - DA.y * DC.x).length() / 2
+
+        in_triangle = True
+        if s_ABD + s_BCD + s_DCA > s_ABC:
+            in_triangle = False
 
 
+        #вычисление расстояния
+        if in_triangle:
+            #print('AAA')
+            return length
+        
+        c_BA_BC = self.translate_to_new_basis(polygon, intersec_point, BA, BC)
+        if c_BA_BC[0] < 0:
+            dist_BA = DB.length()
+        elif c_BA_BC[0] > 1:
+            dist_BA = DA.length()
+        else:
+            dist_BA = Vector(DA.y * BA.z - DA.z * BA.y, DA.z * BA.x - DA.x * BA.z, DA.x * BA.y - DA.y * BA.x).length() / BA.length()
+        if c_BA_BC[1] < 0:
+            dist_BC = DB.length()
+        elif c_BA_BC[1] > 1:
+            dist_BC = DA.length()
+        else:
+            dist_BC = Vector(DB.y * BC.z - DB.z * BC.y, DB.z * BC.x - DB.x * BC.z, DB.x * BC.y - DB.y * BC.x).length() / BC.length()
+        c_CA_CB = self.translate_to_new_basis(polygon, intersec_point, CA, BC * -1)
+        if c_CA_CB[0] < 0:
+            dist_CA = DC.length()
+        elif c_CA_CB[0] > 1:
+            dist_CA = DA.length()
+        else:
+            dist_CA = Vector(DC.y * CA.z - DC.z * CA.y, DC.z * CA.x - DC.x * CA.z, DC.x * CA.y - DC.y * CA.x).length() / CA.length()
+    
+
+        return sqrt(min(dist_BA, dist_BC, dist_CA, DA.length(), DB.length(), DC.length()) ** 2 + length ** 2)
+    
+    def render_polygon(self, polygon):
+        return self.render_points(polygon.points)
+
+class Polygon:
+    def __init__(self, points):
+        self.points = points
+
+    
--- rendering.py
+++ rendering.py
@@ -1,5 +1,5 @@
 import pygame
-from main import Point, Vector, Camera, Edge
+from main import Point, Vector, Camera, Edge, Polygon
 from math import radians
 
 WIDTH = 500  
@@ -18,7 +18,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 
-camera = Camera(Point(0, 0, 0), 0)
+camera = Camera(Point(0, 0, 0), 0, 0)
 
 A = Point(2, -1, 1)
 B = Point(2, 1, 1)
@@ -43,6 +43,12 @@ B1C1 = Edge(B1, C1)
 C1D1 = Edge(C1, D1)
 D1A1 = Edge(D1, A1)
 
+p1 = Point(1, -1, 1)
+p2 = Point(2, 0, 2)
+p3 = Point(1, 1, 1)
+
+POLYGONS = [Polygon([A, B, C]), Polygon([A1, B1, C1])]
+
 running = True
 while running:
     clock.tick(FPS)
@@ -51,9 +57,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-            camera.angle += 3
+            camera.angle_y += 3
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-            camera.angle -= 3
+            camera.angle_y -= 3
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            camera.angle_z += 3
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+            camera.angle_z -= 3
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_w:
             camera.position.x += 1
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
@@ -67,12 +77,21 @@ while running:
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_e:
             camera.position.y -= 1
 
+    #render_edges([AB, BC, CD, DA, AA1, BB1, CC1, DD1, A1B1, B1C1, C1D1, D1A1])
+
     screen.fill(BLACK)
-    for i in camera.render_edges([AB, BC, CD, DA, AA1, BB1, CC1, DD1, A1B1, B1C1, C1D1, D1A1]):
-        pos1 = (i[0][0] * (WIDTH / radians(camera.vision)) + WIDTH // 2, -1 * i[0][1] * (HEIGHT / radians(camera.vision)) + HEIGHT // 2)
-        pos2 = (i[1][0] * (WIDTH / radians(camera.vision)) + WIDTH // 2, -1 * i[1][1] * (HEIGHT / radians(camera.vision)) + HEIGHT // 2)
-        pygame.draw.line(screen, WHITE, pos1, pos2, 5)
+    starting_color = WHITE
+    for i in sorted(POLYGONS, key=lambda x: camera.getDistance(x), reverse=True):
+        points = camera.render_polygon(i)
+        pos1 = (points[0][0] * (WIDTH / radians(camera.vision)) + WIDTH // 2, -1 * points[0][1] * (HEIGHT / radians(camera.vision)) + HEIGHT // 2)
+        pos2 = (points[1][0] * (WIDTH / radians(camera.vision)) + WIDTH // 2, -1 * points[1][1] * (HEIGHT / radians(camera.vision)) + HEIGHT // 2)
+        pos3 = (points[2][0] * (WIDTH / radians(camera.vision)) + WIDTH // 2, -1 * points[2][1] * (HEIGHT / radians(camera.vision)) + HEIGHT // 2)
+        pygame.draw.polygon(screen, starting_color, [pos1, pos2, pos3])
+        starting_color = (starting_color[0] - 100, *starting_color[1:])
     pygame.display.flip()
+    
 
 pygame.quit()
 #TODO: fix edges points that are not rendering
+#TODO: write vector multiplication as vector's magic method
+#TODO: rewrite translation to new basis as a new method
